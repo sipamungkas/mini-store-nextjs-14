0db75ed37203717975a4277aaef226c043c4aa21
sipamungkas <sipamungkas.dev@gmail.com>
2024-10-25 21:29:35 +0700

    style(admin): remove container from admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -104,7 +104,7 @@ export default function AdminLayout({
 
       {/* Main Content */}
       <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
-        <div className="container mx-auto px-6 py-8">
+        <div className="mx-auto px-6 py-8">
           <h3 className="text-3xl font-medium text-gray-700">
             {pathname
               ? pathname.split("/").pop()?.split("-").join(" ").toUpperCase()
